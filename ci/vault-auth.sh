f69557c5956298781e369f8ded18da871f52f764
Alex Leavitt <alexander.d.leavitt@gmail.com>
2019-10-07 14:12:52 -0400

    Fix bash errors

--- ci/vault-auth.sh
+++ ci/vault-auth.sh
@@ -3,12 +3,12 @@
 export VAULT_ADDRESS=http://vault.vault.svc.cluster.local:8200
 vault login --method=userpass username=concourse password=password
 
-for file in vault/app; do
-     ./vault/app/$file
+for file in vault/app/*; do
+     sh $file
 done
 
-for policy in vault/policies; do
-    policyName=$(echo $policy | cut -d'.' -f1)
+for policy in vault/policies/*; do
+    policyName=$(echo $policy | cut -d'.' -f1 | cut -d'/' -f3)
     echo $policyName
-    vault policy write $policyName @vault/policy/$policy
+    vault policy write $policyName @$policy
 done
