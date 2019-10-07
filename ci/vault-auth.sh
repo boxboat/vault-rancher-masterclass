1c6541715f99ca099bc0a3cfdd9f47fd58ac9581
Alex Leavitt <alexander.d.leavitt@gmail.com>
2019-10-07 14:26:59 -0400

    Working on finalizing bash script

--- ci/vault-auth.sh
+++ ci/vault-auth.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-export VAULT_ADDRESS=http://vault.vault.svc.cluster.local:8200
 vault login --method=userpass username=concourse password=password
 
-for file in vault/app/*; do
+for file in vault/apps/*; do
      sh $file
 done
 
 for policy in vault/policies/*; do
     policyName=$(echo $policy | cut -d'.' -f1 | cut -d'/' -f3)
     echo $policyName
-    vault policy write $policyName @$policy
+    cat $policy | vault policy write $policyName -
 done
