a488bd1c5072c6221e9b5737a29efab8e08b48c9
Alex Leavitt <alexander.d.leavitt@gmail.com>
2019-10-07 13:51:00 -0400

    Update ci info

--- ci/vault-auth.sh
+++ ci/vault-auth.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+VAULT_ADDRESS=http://vault.vault.svc.cluster.local:8200
 vault login --method=userpass username=concourse password=password
+
+for file in vault/app; do
+     ./vault/app/$file
+done
+
+for policy in vault/policy; do
+    policyName=$(echo $policy | cut -d'.' -f1)
+    echo $policyName
+    vault policy write $policyName @vault/policy/$policy
+done
