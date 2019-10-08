0e4e18db5641e3f42b39b9bfcc4c2f86b1ba61be
Alex Leavitt <alexander.d.leavitt@gmail.com>
2019-10-08 13:24:53 -0400

    fix typo

--- vault/apps/hello-world.sh
+++ vault/apps/hello-world.sh
@@ -2,4 +2,4 @@ vault write auth/kubernetes/role/hello-world \
     bound_service_account_names=app \
     bound_service_account_namespaces=default \
     policies=app-policy \
-    ttl=1hs
+    ttl=1h
